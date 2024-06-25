9a61c1f186300fd315c76e4cf8334f436e42a3f8
Lucas Henrique Martins <lucasti@live.com>
2024-06-24 23:07:37 -0300

    async task implemented

--- bins/consumers/src/consumer/process.rs
+++ bins/consumers/src/consumer/process.rs
@@ -5,6 +5,7 @@ use services::{
     amqp::{traits::ConsumerHandler, AmqpError, AmqpMessage},
     messages::ProcessDataTimer,
 };
+
 use tokio::time::sleep;
 use tracing::{error, info};
 
@@ -19,25 +20,26 @@ impl ConsumerMessage {
 #[async_trait]
 impl ConsumerHandler for ConsumerMessage {
     async fn handle(&self, msg: AmqpMessage) -> Result<(), AmqpError> {
-        match serde_json::from_slice::<ProcessDataTimer>(&msg.data) {
-            Ok(data) => {
-                info!("Received data to process {}", data);
-                // todo create a implementation to call some code here
-                execute_task(data);
-                return Ok(());
-            }
+        let data = match serde_json::from_slice::<ProcessDataTimer>(&msg.data) {
+            Ok(data) => data,
+
             Err(err) => {
                 error!("Error to retrieve information of message {}", err);
                 return Err(AmqpError::ProcessMessage);
             }
         };
+        info!("Received data to process {}", data);
+
+        tokio::spawn(async move {
+            process_message(data).await;
+        });
+        Ok(())
     }
 }
-async fn execute_task(data: ProcessDataTimer) {
-    println!("Task started: {:?}", data);
-    // Simula o tempo de TTL
+async fn process_message(data: ProcessDataTimer) {
+    info!("Init TTL Await");
+    // wait to execute ttl
     sleep(Duration::from_secs(data.ttl as u64)).await;
-    // Executa a tarefa após o TTL
-    println!("Executing task: {:?}", data);
-    // Aqui você pode colocar a lógica da tarefa que deseja executar
+
+    info!("End TTL await");
 }
